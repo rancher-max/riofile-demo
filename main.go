19003f4ae4dcc434ab50f923935564353209a143
Daishan <daishan@rancher.com>
2019-08-29 10:45:15 -0700

    test

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(100 * time.Millisecond)
 	fmt.Fprintln(w, "Hi there, I am rio:v19238273232")
+	crap
 }
 
 func main() {
