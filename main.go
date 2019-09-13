b5abaae926f782122ab86077aedf9760f7fddcce
Daishan Peng <StrongMonkey@users.noreply.github.com>
2019-09-13 11:01:21 -0700

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(100 * time.Millisecond)
-	fmt.Fprintln(w, "Hi there, I am rio:production88888")
+	fmt.Fprintln(w, "Hi there, I am rio:production99999")
 }
 
 func main() {
